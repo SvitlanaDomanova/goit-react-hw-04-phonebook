9d2d6a3df909dfc3d6cfb1c17b9f7a3460c79e69
SvitlanaDomanova <majba161@gmail.com>
2023-06-08 17:59:37 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
+import ContactForm from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { useState, useEffect } from 'react';
+
+const INITIAL_LIST = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? INITIAL_LIST;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = contact => {
+    const isIncontacts = contacts.some(
+      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+    );
+    if (isIncontacts) {
+      alert(`${contact.name} is already in contacts.`);
+      return;
+    }
+    setContacts(prevState => [...prevState, { id: nanoid(), ...contact }]);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const removeContacts = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
+  };
+  const visibleContacts = getVisibleContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ width: '300px', marginLeft: '15px' }}>
+      <h1>Phonebook</h1>
+
+      <ContactForm onSubmit={addContact} />
+
+      <h2>Contacts </h2>
+
+      {contacts.length > 0 ? (
+        <Filter value={filter} onChange={changeFilter} />
+      ) : (
+        'Your phonebook is empty'
+      )}
+      {contacts.length > 0 && (
+        <ContactList
+          contacts={visibleContacts}
+          onRemoveContact={removeContacts}
+        />
+      )}
     </div>
   );
 };
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,76 @@
+import css from './ContactForm.module.css';
+import { useState } from 'react';
+
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = e => {
+    setName(e.target.value);
+  };
+
+  const handleNumberChange = e => {
+    setNumber(e.target.value);
+  };
+
+  // аналогічний запис як "handleNumberChange, handleNameChange "
+  // const handleChange = e => {
+  //   const { name, number } = e.target;
+  //   switch (name) {
+  //     case 'name':
+  //       setName(value);
+
+  //       break;
+  //     case 'number':
+  //       setNumber(number);
+  //     default:
+  //       console.warn(`тип поля name -$(name) не обробляється`);
+  //   }
+  // };
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ name, number });
+    reset();
+  };
+
+  return (
+    <div>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <label className={css.lable}>
+          Name
+          <input
+            className={css.inputContact}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={handleNameChange}
+          />
+        </label>
+        <label className={css.lable}>
+          Number
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={handleNumberChange}
+          />
+        </label>
+        <button type="submit" className={css.button}>
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
+export default ContactForm;
